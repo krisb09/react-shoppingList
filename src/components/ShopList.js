cd32517ed3524ef267310c60c39dfc9f4f601023
Kris Bautista <krisb09@gmail.com>
2022-02-18 15:06:06 -0500

    updated component list

---
+++ src/components/ShopList.js
@@ -0,0 +1,24 @@
+//displays to do list items on screen
+import React from "react";
+
+const ItemList = ({ todos, setTodos, setEditTodo }) => {
+  const handleEdit = ({ id }) => {
+    const findTodo = todos.find((todo) => todo.id === id);
+    setEditTodo(findTodo);
+  };
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      {/* map thru todos list item and display list item on the screen*/}
+      {todos.map((todo) => (
+        <li className="list-item" key={todo.id}></li>
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
