b436d0385d5cde107a02bbf905ffa884d74003ec
Kris Bautista <krisb09@gmail.com>
2022-02-18 15:03:37 -0500

    added new component

---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { v4 as uuidV4 } from "uuid";
+import "../styles/Form.css";
+
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) =>
+      todo.id === id ? { title, id, completed } : todo
+    );
+    setTodos(newTodo);
+    setEditTodo("");
+  };
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if (!editTodo) {
+      setTodos([...todos, { id: uuidV4(), title: input, completed: false }]);
+      setInput("");
+    } else {
+      updateTodo(input, editTodo.id, editTodo.completed);
+    }
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter item"
+        className="task-input"
+        value={input}
+        required
+        onChange={onInputChange}
+      />
+    </form>
+  );
+};
+
+export default Form;
